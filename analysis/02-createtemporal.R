8f5002996b82ca60102fcbe3d0b1dddcbc460852
Martina Fonseca <martina.fonseca@nhsx.nhs.uk>
2021-03-23 16:21:33 +0000

    Force subdirectory creation in 02 too

--- analysis/02-createtemporal.R
+++ analysis/02-createtemporal.R
@@ -25,6 +25,29 @@ library(svglite)
 if (!dir.exists(here::here("output", "plots"))){
   dir.create(here::here("output", "plots"))
 }
+# create directory for saving plots, if not existent
+if (!dir.exists(here::here("output", "tables"))){
+  dir.create(here::here("output", "tables"))
+}
+
+
+## Redactor code (W.Hulme)
+redactor <- function(n, threshold=6,e_overwrite=NA_integer_){
+  # given a vector of frequencies, this returns a boolean vector that is TRUE if
+  # a) the frequency is <= the redaction threshold and
+  # b) if the sum of redacted frequencies in a) is still <= the threshold, then the
+  # next largest frequency is also redacted
+  n <- as.integer(n)
+  leq_threshold <- dplyr::between(n, 1, threshold)
+  n_sum <- sum(n)
+  # redact if n is less than or equal to redaction threshold
+  redact <- leq_threshold
+  # also redact next smallest n if sum of redacted n is still less than or equal to threshold
+  if((sum(n*leq_threshold) <= threshold) & any(leq_threshold)){
+    redact[which.min(dplyr::if_else(leq_threshold, n_sum+1L, n))] = TRUE
+  }
+  n_redacted <- if_else(redact, e_overwrite, n)
+}
 
 # create look-up table to iterate over
 md_tbl <- tibble(
@@ -53,10 +76,12 @@ p_saving <- function(id,data) {
 # Create redacted measures and save
 measures <- measures %>%
   mutate(
-    redacted_data = pmap(lst(id,data),
-                      function(id,data) {
-                        write.csv(data,paste0(here::here("output","tables"),"/redacted_measure_",id,".csv"))
-                        return(data)
+    redacted_data = pmap(lst(id,measure_col,data),
+                      function(id,measure_col,data) {
+                        redacted_data <- data %>% mutate_at(vars(measure_col),redactor)
+                        redacted_data$value <- ifelse(is.na(redacted_data %>% select(measure_col)),NA,redacted_data$value)
+                        write.csv(redacted_data,paste0(here::here("output","tables"),"/redacted_measure_",id,".csv"))
+                        return(redacted_data)
                       }
                       )
   )
