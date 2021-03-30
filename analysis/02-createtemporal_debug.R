16b2162daa6b57f4fc380ddcf28076ab2069e163
Martina Fonseca <martina.fonseca@nhsx.nhs.uk>
2021-03-30 09:15:29 +0100

    Commit to what should be a working version, before jumping to snomed

--- analysis/02-createtemporal_debug.R
+++ analysis/02-createtemporal_debug.R
@@ -13,13 +13,14 @@
 # 
 ## ==============================================================================
 
-## open log connection to file
-sink(here::here("logs", "log-02-createtemporal-debug.txt"))
 ## library
 library(tidyverse)
 library(here)
 library(svglite)
+## open log connection to file
+sink(here::here("logs", "log-02-createtemporal-debug.txt"))
 print("> Libraries")
+
 
 # create directory for saving plots, if not existent
 if (!dir.exists(here::here("output", "plots"))){
@@ -75,11 +76,11 @@ print("> Redactor def")
 #   denominator = "population",
 #   group_by = rep("practice",1,n_meas)
 # )
-n_meas=5
+n_meas=6
 md_tbl <- tibble(
-  measure = c("gpc", "OC_Y1f3b","OC_Y22b4","OC_XaXcK","OC_XUman"),
-  measure_col=c("gp_consult_count", "OC_Y1f3b","OC_Y22b4","OC_XaXcK","OC_XUman"),
-  measure_label = c("GPconsult", "OC_Y1f3b","OC_Y22b4","OC_XaXcK","OC_XUman"),
+  measure = c("gpc", "OC_Y1f3b","OC_Y22b4","OC_XaXcK","OC_computerlink","OC_alertreceived"),
+  measure_col=c("gp_consult_count", "OC_Y1f3b","OC_Y22b4","OC_XaXcK","OC_computerlink","OC_alertreceived"),
+  measure_label = c("GPconsult", "OC_Y1f3b","OC_Y22b4","OC_XaXcK","OC_computerlink","OC_alertreceived"),
   by = rep("practice",1,n_meas),
   by_label = rep("by practice",1,n_meas),
   id = paste0(measure, "_", by),
