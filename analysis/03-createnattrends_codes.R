e4f011b14b39c2f38b82746efa3a26215de335d5
Martina Fonseca <martina.fonseca@nhsx.nhs.uk>
2021-04-06 19:54:08 +0100

    reducing memory demands in 03. Both ctv3 and OC given

--- analysis/03-createnattrends_codes.R
+++ analysis/03-createnattrends_codes.R
@@ -60,38 +60,44 @@ print("Redactor defined")
 
 df_input <- read_csv(
   here::here("output","measures",paste0("input_measures_bycode_",  query_dates[1], ".csv")))
-df_input <- df_input %>% mutate(month=query_dates[1])
+df_input <- df_input %>% mutate(population=1) %>% group_by(region,stp,practice) %>% summarise_all(~sum(.,na.rm=T)) %>% ungroup()
+df_input <- df_input %>% mutate(month=query_dates[1]) %>% select(-patient_id)
 print(here::here("output","measures",paste0("input_measures_bycode_",  query_dates[1], ".csv")))
 
 for (datenow in tail(query_dates,-1)){
-  print(here::here("output","measures",paste0("input_measures_bycode_",  datenow, ".csv")))
   df_input_now <- read_csv(
     here::here("output","measures",paste0("input_measures_bycode_",  datenow, ".csv")))
+  print(here::here("output","measures",paste0("input_measures_bycode_",  datenow, ".csv")))
   print("loaded")
+  df_input_now <- df_input_now %>% mutate(population=1) %>% group_by(region,stp,practice) %>% summarise_all(~sum(.,na.rm=T)) %>% ungroup()
+  print("aggregated")
+  df_input_now <- df_input_now %>% mutate(month=datenow) %>% select(-patient_id)
   df_input_now <- df_input_now %>% mutate(month=datenow)
   print("mutated")
   df_input <- df_input %>% bind_rows(df_input_now)
   print("bound to master")
 }
 df_input <- as.data.frame(df_input)
-df_input <- df_input %>% rename(snomedc_gp_consult_count=gp_consult_count)
+#df_input <- df_input %>% rename(snomedc_gp_consult_count=gp_consult_count)
 print(df_input %>% select_if(is.numeric) %>% summarise_all(~sum(.,na.rm=T)))
 print(summary(df_input))
 print(head(df_input,0))
 rm(df_input_now)
 
-myprefix="snomed"
-
 df_summary <- df_input %>%
   group_by(month) %>%
-  summarise_at(vars(starts_with(myprefix)),~sum(.,na.rm=T))
+  summarise_at(vars(starts_with("snomed"),starts_with("OC"),population,gp_consult_count),~sum(.,na.rm=T))
 print("summary created -a")
-df_summary_pop <- df_input %>% group_by(month) %>% summarise(OC_population=n_distinct(patient_id))
-print("summary created -b")
-df_summary <- left_join(df_summary,df_summary_pop,id="month")
-print("summary created -c")
+#df_summary_pop <- df_input %>% group_by(month) %>% summarise(OC_population=n_distinct(patient_id))
+#print("summary created -b")
+#df_summary <- left_join(df_summary,df_summary_pop,id="month")
+#print("summary created -c")
 
-rm(df_summary_pop)
+#rm(df_summary_pop)
+
+
+#### SNOMED - practice coverage ###
+myprefix="snomed"
 
 ## Calculations for practice coverage
 df_practice_flags <- df_input %>% group_by(region,stp,practice) %>% summarise_at(vars(starts_with(myprefix)),~ifelse(sum(.,na.rm=T)>0,1,0))
@@ -141,15 +147,67 @@ print("practice fig04 created")
 ggsave(paste0(here::here("output","plots"),"/sc03_fig04_pracbyregcoverage.svg"),width = 30, height = 30, dpi=300,units ="cm")
 print("practice fig04 saved")
 
-rm(df_input)
+#rm(df_input)
 rm(df_practice_flags)
 rm(tbx_practice_flags_)
 rm(tbx_practice_flags_reg)
 
 
-## National trends
+#### CTV3 - practice coverage ###
+myprefix="OC"
 
-df_summary_long <- df_summary %>% pivot_longer(cols=starts_with(myprefix),
+## Calculations for practice coverage
+df_practice_flags <- df_input %>% group_by(region,stp,practice) %>% summarise_at(vars(starts_with(myprefix)),~ifelse(sum(.,na.rm=T)>0,1,0))
+print("practice calc 1")
+
+tbx_practice_flags_ <- pivot_longer(df_practice_flags,cols=starts_with(myprefix),
+                                    names_to="code",
+                                    values_to="had_instance")
+print("practice calc 2")
+
+tbx_practice_flags_reg <- tbx_practice_flags_ %>%
+  group_by(region,code) %>%
+  summarise(Present=sum(had_instance),Absent=n()-Present) %>%
+  pivot_longer(c("Present","Absent"),names_to="Instance presence",values_to="no_practices") %>%
+  mutate(code=substr(code,nchar(myprefix)+2,nchar(code)))
+print("practice calc 3")
+
+tbx_practice_flags_ <- tbx_practice_flags_ %>%
+  group_by(code) %>%
+  summarise(Present=sum(had_instance),Absent=n()-Present) %>%
+  pivot_longer(c("Present","Absent"),names_to="Instance presence",values_to="no_practices") %>%
+  mutate(code=substr(code,nchar(myprefix)+2,nchar(code)))
+print("practice calc 4")
+
+tbx_practice_flags_$`Instance presence` <- factor(tbx_practice_flags_$`Instance presence`)
+print("practice calc 5")
+
+ggplot(tbx_practice_flags_, aes(fill=`Instance presence`,x=code, y=no_practices,label=no_practices)) +
+  geom_bar( stat="identity")+
+  geom_text(aes(vjust=0),position = position_stack(vjust = 0.2))+
+  theme(axis.text.x = element_text(angle = -90),text = element_text(size=15))+
+  labs(title="Portion of practices with code recorded",y="Count of practices",x="Code")+
+  coord_flip()
+print("practice fig05 created")
+
+ggsave(paste0(here::here("output","plots"),"/sc03_fig05_ctv3pracnatcoverage.svg"),width = 30, height = 20, dpi=300,units ="cm")
+print("practice fig05 saved")
+
+ggplot(tbx_practice_flags_reg, aes(fill=`Instance presence`,x=code, y=no_practices,label=no_practices)) +
+  geom_bar( stat="identity")+
+  geom_text(aes(hjust=-1),position = position_stack(vjust = 0.0))+
+  theme(axis.text.x = element_text(angle = -90),text = element_text(size=15))+
+  labs(title="Portion of practices with code recorded",y="Count of practices",x="Code")+facet_wrap(~region)+
+  coord_flip()
+print("practice fig06 created")
+
+ggsave(paste0(here::here("output","plots"),"/sc03_fig06_ctv3pracbyregcoverage.svg"),width = 30, height = 30, dpi=300,units ="cm")
+print("practice fig06 saved")
+
+
+#### National trends - snomed ####
+myprefix="snomed"
+df_summary_long <- df_summary %>% pivot_longer(cols=c("gp_consult_count",union(starts_with("snomed"),starts_with("oc"))),
                names_to="Code",
                values_to="Count")
 print("national calc 1")
@@ -160,8 +218,8 @@ print("national calc saved")
 # Disclosiveness: national monthly tally of clinical code occurrence, not deemed disclosive. 
 
 df_summary_long$month <- as.Date(df_summary_long$month)
-df_summary_long <- df_summary_long %>% mutate(rate_per_1000=Count/OC_population*1000)
-ggplot(data=df_summary_long,aes(x=month,y=rate_per_1000,group=Code)) +
+df_summary_long <- df_summary_long %>% mutate(rate_per_1000=Count/population*1000)
+ggplot(data=df_summary_long %>% filter(Code=="gp_consult_count"|substr(Code,1,nchar(myprefix))==myprefix),aes(x=month,y=rate_per_1000,group=Code)) +
   geom_bar(stat="identity",fill="#56B4E9") +
   facet_wrap(~Code,nrow=4,scales="free_y") +
   labs(y="Instance rate (per 1,000)")+
@@ -173,7 +231,7 @@ ggsave(paste0(here::here("output","plots"),"/sc03_fig01_nattrends.svg"),width = 
 print("national fig01 saved")
 # Disclosiveness: plot of national monthly tally of clinical code occurrence, not deemed disclosive. 
 
-ggplot(data=df_summary_long %>% filter(Code%!in% c("snomedc_gp_consult_count")),aes(x=month,y=rate_per_1000,color=Code)) +
+ggplot(data=df_summary_long %>% filter(substr(Code,1,nchar(myprefix))==myprefix),aes(x=month,y=rate_per_1000,color=Code)) +
    geom_line()+
   scale_x_date(date_breaks = "2 months",expand=c(0,0))+
   labs(y="Instance rate (per 1,000)")+
@@ -182,6 +240,44 @@ print("national fig02 created")
 
 ggsave(paste0(here::here("output","plots"),"/sc03_fig02_nattrends.svg"),width = 40, height = 20, dpi=300,units ="cm")
 print("national fig02 saved")
+# Disclosiveness: plot of national monthly tally of clinical code occurrence, not deemed disclosive. 
+
+
+#### National trends - ctv3 ####
+myprefix="OC"
+df_summary_long <- df_summary %>% pivot_longer(cols=c("gp_consult_count",union(starts_with("snomed"),starts_with("oc"))),
+                                               names_to="Code",
+                                               values_to="Count")
+print("national calc 1")
+df_summary_long$Count <- redactor(df_summary_long$Count,threshold =6,e_overwrite=NA_integer_)
+print("national calc2")
+write.csv(df_summary_long,paste0(here::here("output","tables"),"/sc03_tb01_nattrends.csv"))
+print("national calc saved")
+# Disclosiveness: national monthly tally of clinical code occurrence, not deemed disclosive. 
+
+df_summary_long$month <- as.Date(df_summary_long$month)
+df_summary_long <- df_summary_long %>% mutate(rate_per_1000=Count/population*1000)
+ggplot(data=df_summary_long %>% filter(Code=="gp_consult_count"|substr(Code,1,nchar(myprefix))==myprefix),aes(x=month,y=rate_per_1000,group=Code)) +
+  geom_bar(stat="identity",fill="#56B4E9") +
+  facet_wrap(~Code,nrow=4,scales="free_y") +
+  labs(y="Instance rate (per 1,000)")+
+  scale_x_date(date_breaks = "2 months",expand=c(0,0))  +
+  theme(axis.text.x = element_text(angle = -90,vjust = 0))
+print("national fig07 created")
+
+ggsave(paste0(here::here("output","plots"),"/sc03_fig07_ctv3nattrends.svg"),width = 30, height = 30, dpi=300,units ="cm")
+print("national fig07 saved")
+# Disclosiveness: plot of national monthly tally of clinical code occurrence, not deemed disclosive. 
+
+ggplot(data=df_summary_long %>% filter(substr(Code,1,nchar(myprefix))==myprefix),aes(x=month,y=rate_per_1000,color=Code)) +
+  geom_line()+
+  scale_x_date(date_breaks = "2 months",expand=c(0,0))+
+  labs(y="Instance rate (per 1,000)")+
+  theme(axis.text.x = element_text(angle = -90,vjust = 0))
+print("national fig08 created")
+
+ggsave(paste0(here::here("output","plots"),"/sc03_fig08_ctv3nattrends.svg"),width = 40, height = 20, dpi=300,units ="cm")
+print("national fig08 saved")
 # Disclosiveness: plot of national monthly tally of clinical code occurrence, not deemed disclosive. 
 
 
